bc7b65211e86beb9398243cf1ad43f308a4777ba
lambda-san7 <codekiwi132@gmail.com>
2023-10-09 11:01:39 -0700

    this

--- Tic Tac Toe/Tic Tac Toe.cpp
+++ Tic Tac Toe/Tic Tac Toe.cpp
@@ -90,25 +90,82 @@ public:
 new_grid grid;
 
 ////////////////////////
+// CHECK WIN
+////////////////////////
+
+bool win(std::string turn) {
+
+    std::cout << "called win() \n";
+
+    // HORIZONTAL
+
+    int count = 0;
+
+    for (int row = 0; row < 3; row++) {
+        count = 0;
+        for (int col = 0; col < 3; col++) {
+            if (grid.matrix[row][col] == turn) {
+                count += 1;
+            }
+        }
+    }
+    if (count == 3) {
+        return true;
+    }
+    if (count < 3) {
+        return false;
+    }
+
+    for (int row = 0; row < 3; row++) {
+        count = 0;
+        for (int col = 0; col < 3; col++) {
+            if (grid.matrix[row][col] == turn) {
+                count += 1;
+            }
+        }
+    }
+}
+
+////////////////////////
 // GAME
 ////////////////////////
 
-std::string turn = "x";
+std::string turn = "X";
 
 void switch_turn() {
     if (turn == "X") {
         turn = "O";
-    } else {
+    }
+    else {
         turn = "X";
     }
 }
 
 ////////////////////////
+// SCENES PROTOTYPES
+////////////////////////
+
+void main_menu();
+void game();
+void game_end();
+
+////////////////////////
 // SCENES
 ////////////////////////
 
+void game_end(std::string winner) {
+    system("cls");
+    std::cout << "+-----------+ \n";
+    std::cout << "| " << winner << " Wins!   | \n";
+    std::cout << "+-----------+ \n";
+    std::cout << "| 1) Re-Play| \n";
+    std::cout << "| 2) Menu   | \n";
+    std::cout << "+-----------+ \n";
+    system("pause");
+    main_menu();
+}
+
 void game() {
-    switch_turn();
     system("cls");
     std::cout << turn + "'s Turn \n";
     grid.render("row");
@@ -119,6 +176,10 @@ void game() {
     int col = intput("$col>");
     if (grid.matrix[row - 1][col - 1] == "   ") {
         grid.matrix[row - 1][col - 1] = " " + turn + " ";
+        if (win(" " + turn + " ")) {
+            game_end(turn);
+        }
+        switch_turn();
     }
     game();
 }
@@ -126,7 +187,7 @@ void game() {
 void main_menu() {
     system("cls");
     std::cout << "+-----------+ \n";
-    std::cout << "| Main Menu | \n";
+    std::cout << "| Main Menu | NEW VERSION \n";
     std::cout << "+-----------+ \n";
     std::cout << "| 1) Play   | \n";
     std::cout << "| 2) Exit   | \n";
